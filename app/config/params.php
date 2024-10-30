9f7ed4ec31e880564b9de637c1a4e8ca72b79faf
RifSalah <Salah.devrif@hotmail.com>
2024-10-30 15:41:48 +0100

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,7 @@
+<?php
+// PARAMS
+// Paramètres de connexion à la DB
+define('DB_HOST','localhost:3306');
+define('DB_NAME','alex_parker_octobre_2021');
+define('DB_USER','root');
+define('DB_PASSWORD','root');
